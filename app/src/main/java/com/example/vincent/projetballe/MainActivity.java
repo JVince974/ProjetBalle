6abf19bbb6dbc43507932d2bafc89ed40833839d
JVince974 <vincent.joabsing@gmail.com>
2017-10-10 15:48:21 +0400

    Ajout des nouvelles classes JAVA

--- app/src/main/java/com/example/vincent/projetballe/MainActivity.java
+++ app/src/main/java/com/example/vincent/projetballe/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.vincent.projetballe;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,36 +17,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+
+    /************************
+     * GESTION DE L'OPTION
+     ************************/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_LONG);
+            return true;
+        } else if (id == R.id.action_scores) {
+            Intent intent = new Intent(this, ScoresActivity.class);
+            startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /************************
+     * LANCER LE JEU
+     ************************/
+    public void startGame(View view) {
+        // GameActivity
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
