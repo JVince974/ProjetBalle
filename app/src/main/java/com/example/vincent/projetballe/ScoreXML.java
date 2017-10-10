dcdf1efa93759526bf3febe58737f618349750b0
JVince974 <vincent.joabsing@gmail.com>
2017-10-10 15:20:20 +0400

    Ajout des nouvelles classes JAVA

--- app/src/main/java/com/example/vincent/projetballe/ScoreXML.java
+++
@@ -1,8 +0,0 @@
-package com.example.vincent.projetballe;
-
-/**
- * Created by Vincent on 10/10/2017.
- */
-
-public class ScoreXML {
-}
---
+++ app/src/main/java/com/example/vincent/projetballe/outils/Localisation.java
@@ -0,0 +1,8 @@
+package com.example.vincent.projetballe.outils;
+
+/**
+ * Created by Vincent on 10/10/2017.
+ */
+
+public class Localisation {
+}
