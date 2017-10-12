19f1bbd43089f8457ec1ee7b84b2eb668e77f26f
JVince974 <vincent.joabsing@gmail.com>
2017-10-12 16:43:58 +0400

    Implémentation des Threads

--- app/src/main/java/com/example/vincent/projetballe/model/LesBalles/IABalle.java
+++ app/src/main/java/com/example/vincent/projetballe/model/LesBalles/IABalle.java
@@ -14,10 +14,18 @@ import java.util.Random;
 public class IABalle extends Balle implements Runnable {
 
     // ia color = black
-    protected static int color = Color.BLACK;
+    private final static int color = Color.BLACK;
+
+    private final static int NORTH_WEST = 0;
+    private final static int NORTH_EAST = 1;
+    private final static int SUD_EAST = 2;
+    private final static int SUD_WEST = 3;
+
+    private int direction;
 
     private IABalle(int x, int y, int radius) {
         super(x, y, radius, color);
+        direction = (int) (Math.random() * 4); // choisir une direction aléatoire
     }
 
     // les balles ia apparaissent aléatoire en fonction de la position de l'user balle
@@ -31,11 +39,13 @@ public class IABalle extends Balle implements Runnable {
     @Override
     public void run() {
         while (true) {
-            Random r = new Random();
-            int x = (int) radius + r.nextInt((GameView.viewWidth - radius) - radius);
-            int y = (int) radius + r.nextInt((GameView.viewHeight + radius) - radius);
-            this.x = x;
-            this.y = y;
+
+//            Random r = new Random();
+//            int x = (int) radius + r.nextInt((GameView.viewWidth - radius) - radius);
+//            int y = (int) radius + r.nextInt((GameView.viewHeight + radius) - radius);
+//            this.x = x;
+//            this.y = y;
+
 
         }
     }
