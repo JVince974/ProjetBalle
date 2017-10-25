659f0448b9f1fb196939a9be6f8654804778d5b0
JVince974 <vincent.joabsing@gmail.com>
2017-10-25 14:54:40 +0400

    Implémentation de la classe GPSTracking qui récupère la position de l'utilisateur

---
+++ app/src/main/java/com/example/vincent/projetballe/bibliotheque/GPSTracking.java
@@ -0,0 +1,8 @@
+package com.example.vincent.projetballe.bibliotheque;
+
+/**
+ * Cette classe permet de récupérer la localisation du joueur
+ * Assurez-vous d'avoir la permission de localisation
+ */
+public class GPSTracking {
+}
